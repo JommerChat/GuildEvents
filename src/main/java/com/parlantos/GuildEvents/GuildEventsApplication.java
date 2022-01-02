fa39db629d481231988d38b5297b3f637129e0af
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-02 13:06:54 -0500

    Initial commit of template project for GuildEvents

---
+++ src/main/java/com/parlantos/GuildEvents/GuildEventsApplication.java
@@ -0,0 +1,13 @@
+package com.parlantos.GuildEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildEventsApplication.class, args);
+	}
+
+}
